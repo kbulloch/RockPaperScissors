0e385dd1c520bc9592556f557e54b26507e01ca0
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-11 10:00:23 -0700

    Add test for paper vs rock

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -6,8 +6,16 @@
         function runGame($p1_input, $p2_input)
         {
 
-            if($p1_input === $p2_input){
+            if ($p1_input === $p2_input) {
                 return "DRAW";
+            }
+
+            if ($p1_input === "PAPER") {
+                switch ($p2_input) {
+                    case "ROCK":
+                        return "Player 1 wins";
+                        break;
+                }
             }
 
         }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -49,6 +49,34 @@
             $this->assertEquals("DRAW", $result);
         }
 
+        function test_RockPaperScissors_PaperVsRock()
+        {
+            //Arrange
+            $paper_vs_rock = new RockPaperScissors;
+            $p1_input = "PAPER";
+            $p2_input = "ROCK";
+
+            //Act
+            $result = $paper_vs_rock->runGame($p1_input, $p2_input);
+
+            //Assert
+            $this->assertEquals("Player 1 wins", $result);
+        }
+
+        function test_RockPaperScissors_ScissorsVsPaper()
+        {
+            //Arrange
+            $scissors_vs_paper = new RockPaperScissors;
+            $p1_input = "SCISSORS";
+            $p2_input = "PAPER";
+
+            //Act
+            $result = $scissors_vs_paper->runGame($p1_input, $p2_input);
+
+            //Assert
+            $this->assertEquals("Player 1 wins", $result);
+        }
+
     }
 
 ?>
